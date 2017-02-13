68bcca481fe0669474f2c7877d307f254a507936
astinchoi <asciineo@gmail.com>
2017-02-13 09:09:05 +0900

    iterator pattern

---
+++ 1_iterator/Main.java
@@ -0,0 +1,14 @@
+public class Main {
+    public static void main(String[] args) {
+        BookShelf bookShelf = new BookShelf(4);
+        bookShelf.appendBook(new Book("Alchemist"));
+        bookShelf.appendBook(new Book("Redis"));
+        bookShelf.appendBook(new Book("Machine Learning"));
+        bookShelf.appendBook(new Book("Chatting Bot"));
+        Iterator it = bookShelf.iterator();
+        while (it.hasNext()) {
+            Book book = (Book)it.next();
+            System.out.println(book.getName());
+        }
+    }
+}
