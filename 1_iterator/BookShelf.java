ca0b053bff10f8c120a869d00875e75078df088f
astinchoi <asciineo@gmail.com>
2017-02-14 13:29:13 +0900

    homework

--- 1_iterator/BookShelf.java
+++ 1_iterator/BookShelf.java
@@ -1,19 +1,31 @@
+import java.util.ArrayList;
+
+
 public class BookShelf implements Aggregate {
-    private Book[] books;
+    // private Book[] books;
+    private ArrayList<Book> books;
     private int last = 0;
     
-    public BookShelf(int maxsize) {
-        this.books = new Book[maxsize];
+    public BookShelf() {
+        this.books = new ArrayList<Book>();
     }
+
+    // public BookShelf(int maxsize) {
+    //     this.books = new Book[maxsize];
+    // }
+
     public Book getBookAt(int index) {
-        return books[index];
+        // return books[index];
+        return this.books.get(index);
     }
     public void appendBook(Book book) {
-        this.books[last] = book;
-        last++;
+        // this.books[last] = book;
+        // last++;
+        this.books.add(book);
     }
     public int getLength() {
-        return last;
+        // return last;
+        return this.books.size();
     }
     public Iterator iterator() {
         return new BookShelfIterator(this);
--- 1_iterator/Main.java
+++ 1_iterator/Main.java
@@ -1,10 +1,13 @@
 public class Main {
     public static void main(String[] args) {
-        BookShelf bookShelf = new BookShelf(4);
+        // BookShelf bookShelf = new BookShelf(4);
+        BookShelf bookShelf = new BookShelf();
+
         bookShelf.appendBook(new Book("Alchemist"));
         bookShelf.appendBook(new Book("Redis"));
         bookShelf.appendBook(new Book("Machine Learning"));
         bookShelf.appendBook(new Book("Chatting Bot"));
+
         Iterator it = bookShelf.iterator();
         while (it.hasNext()) {
             Book book = (Book)it.next();
